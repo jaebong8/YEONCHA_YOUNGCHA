363ddf3718a91d992f0271f328cdf39a40cc53f7
jaebong8 <jaebong90@naver.com>
2023-02-06 18:19:28 +0900

    달력 전 달, 다음 달, 클릭 버튼

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -5,11 +5,7 @@ export default function Document() {
         <Html lang="en">
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link
-                    rel="preconnect"
-                    href="https://fonts.gstatic.com"
-                    crossOrigin
-                />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;700&family=Noto+Sans:wght@100;300;400;700&display=swap"
                     rel="stylesheet"
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -4,15 +4,19 @@ import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import { useRouter } from "next/router";
 import styles from "../styles/home.module.scss";
 import Image from "next/image";
+import { format, addMonths, subMonths } from "date-fns";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 const Home: React.FC = () => {
     const auth = getAuth();
     const router = useRouter();
+    const [today, setToday] = useState(new Date());
     const [loading, setLoading] = useState(true);
+    const weekMock = ["일", "월", "화", "수", "목", "금", "토"];
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
             if (user) {
-                console.log(user);
                 setLoading(false);
             } else {
                 console.log("logout");
@@ -23,6 +27,13 @@ const Home: React.FC = () => {
     const onLogout = useCallback(() => {
         signOut(auth);
     }, []);
+
+    const nextMonthHandler = useCallback(() => {
+        setToday(addMonths(today, 1));
+    }, [today]);
+    const prevMonthHandler = useCallback(() => {
+        setToday(subMonths(today, 1));
+    }, [today]);
 
     return (
         <>
@@ -47,13 +58,33 @@ const Home: React.FC = () => {
                         </nav>
                         <main>
                             <div className={styles.topBar}>
-                                <div className={styles.left}></div>
+                                <div className={styles.left}>
+                                    <div className={styles.fullyearTitle}>{format(new Date(), "yyyy년 MM월 dd일")}</div>
+                                </div>
                                 <h2>연차 사용 현황</h2>
                                 <div className={styles.avatar}>
-                                    {" "}
                                     <button onClick={onLogout}>로그아웃</button>
                                 </div>
                             </div>
+                            <section className={styles.calendarContainer}>
+                                <div className={styles.calendar}>
+                                    <div className={styles.yearTitle}>{format(today, "yyyy년")}</div>
+                                    <div className={styles.monthTitle}>
+                                        <button className={styles.prevButton} onClick={prevMonthHandler}>
+                                            <FontAwesomeIcon icon={faChevronLeft} />
+                                        </button>
+                                        <div className={styles.month}>{format(today, "M월")}</div>
+                                        <button className={styles.nextButton} onClick={nextMonthHandler}>
+                                            <FontAwesomeIcon icon={faChevronRight} />
+                                        </button>
+                                    </div>
+                                    <div className={styles.dayContainer}>
+                                        {weekMock.map((v) => {
+                                            return <div>{v}</div>;
+                                        })}
+                                    </div>
+                                </div>
+                            </section>
                         </main>
                     </div>
                 </>
